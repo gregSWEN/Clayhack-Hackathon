2be5b4f206dfc8d82c0658c984b213a1e9206ae2
SaranyaDadi <71734141+SaranyaDadi@users.noreply.github.com>
2020-09-26 11:59:13 -0400

    Add files via upload

--- API.py
+++ API.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-response = requests.get("http://api.open-notify.org/astros.json")
+response = requests.get("http://api.open-notofy.org/iss-pass.json")
 # print(response.status_code)
 # print(response.json())
 
@@ -12,5 +12,14 @@ def jprint(obj):
     print(text)
 
 
-jprint(response.json())
+"""response2 = requests.get("http://api.open-notofy.org/iss-pass.json")
+
+
+def jprint2(obj2):
+    cord = json.dumps(obj2, sort_keys=True, indent=3)
+    print(cord)
+
+
+jprint2(response.json())
+"""
 
