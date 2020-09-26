a8e92da1fc8c2d3cbdd06b9e73dbda0b2715dd75
SaranyaDadi <71734141+SaranyaDadi@users.noreply.github.com>
2020-09-26 12:35:13 -0400

    Add files via upload

--- API.py
+++ API.py
@@ -13,16 +13,39 @@ def jprint(obj):
 
 jprint(response.json())
 
+parameters = {
+    "lat": 40.71,
+    "lon": -74
+}
 
-"""response2 = requests.get("http://api.open-notofy.org/iss-pass.json", params = parameters)
+response2 = requests.get(" http://api.open-notify.org/iss-pass.json", params=parameters)
 print(response2.status_code)
-print(response2.json)
-    parameters = {
-        "lat": 40.71,
-        "lon": -74
-    }
+#print(response.json())
 
 
-jprint2(response2.json())"""
+def jprint2(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint2(response2.json())
+
+pass_times = response2.json()['response']
+jprint2(pass_times)
+
+risetimes = []
+
+for d in pass_times:
+    time = d['risetime']
+    risetimes.append(time)
+
+print(risetimes)
+
+from datetime import datetime
+times = []
+for rt in risetimes:
+    time = datetime.fromtimestamp(rt)
+    times.append(time)
+    print(time)
 
 
